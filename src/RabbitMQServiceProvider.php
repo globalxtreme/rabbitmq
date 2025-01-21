cc530d778401c90f813b2f477dd4c3427caf9ef2
yuswa-arba <yuswa98@gmail.com>
2025-01-21 11:46:27 +0800

    Add constant and default consumer command for global & local connection

--- src/RabbitMQServiceProvider.php
+++ src/RabbitMQServiceProvider.php
@@ -11,5 +11,10 @@ class RabbitMQServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/../config/gx-rabbitmq' => config_path('gx-rabbitmq.php'),
         ], 'gx-rabbitmq-config');
+
+        $this->publishes([
+            __DIR__.'/Commands/RabbitMQGlobalCommand' => app_path('/Console/Commands/MessageBroker/RabbitMQGlobalCommand.php'),
+            __DIR__.'/Commands/RabbitMQLocalCommand' => app_path('/Console/Commands/MessageBroker/RabbitMQLocalCommand.php'),
+        ], 'gx-rabbitmq-command');
     }
 }
